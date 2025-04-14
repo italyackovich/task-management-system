5872614b68a2add7e5970a8a837211b14b6a4f9d
Ilya Vehov <vehov.3@gmail.com>
2025-04-14 19:59:10 +0400

    refactor(auth): add cors config

--- src/main/java/ru/kstn/taskmanagementsystem/auth/security/config/SecurityConfig.java
+++ src/main/java/ru/kstn/taskmanagementsystem/auth/security/config/SecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.cors.CorsConfigurationSource;
 import ru.kstn.taskmanagementsystem.auth.security.JwtAuthFilter;
 
 @Configuration
@@ -27,9 +28,9 @@ public class SecurityConfig {
     private final LogoutHandler logoutHandler;
 
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http, CorsConfigurationSource corsConfigurationSource) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable);
-        http.cors(AbstractHttpConfigurer::disable);
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource));
         http.authorizeHttpRequests((requests) -> requests
                 .requestMatchers("/api/v1/auth/**", "/swagger-ui/**", "/v3/**").permitAll()
                 .requestMatchers("/api/v1/admin/**").hasRole("ADMIN")
